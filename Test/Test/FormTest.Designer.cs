36b53ccdd714e8737a22f259bda71a6054bfd21c
Setet <Franklin03res@gmail.com>
2021-04-11 20:23:49 +0300

    v1.00

---
+++ Test/Test/FormTest.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace Test
+{
+    partial class FormTest
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NumberOfQuestions = new System.Windows.Forms.TextBox();
+            this.WritePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ListOfSurnames = new System.Windows.Forms.RichTextBox();
+            this.button_ToAccept = new System.Windows.Forms.Button();
+            this.button_GenerateTest = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NumberOfQuestions
+            // 
+            this.NumberOfQuestions.Location = new System.Drawing.Point(12, 23);
+            this.NumberOfQuestions.Name = "NumberOfQuestions";
+            this.NumberOfQuestions.Size = new System.Drawing.Size(100, 20);
+            this.NumberOfQuestions.TabIndex = 0;
+            // 
+            // WritePath
+            // 
+            this.WritePath.Location = new System.Drawing.Point(12, 80);
+            this.WritePath.Name = "WritePath";
+            this.WritePath.Size = new System.Drawing.Size(230, 20);
+            this.WritePath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // ListOfSurnames
+            // 
+            this.ListOfSurnames.AutoWordSelection = true;
+            this.ListOfSurnames.Location = new System.Drawing.Point(248, 7);
+            this.ListOfSurnames.Name = "ListOfSurnames";
+            this.ListOfSurnames.Size = new System.Drawing.Size(187, 197);
+            this.ListOfSurnames.TabIndex = 3;
+            this.ListOfSurnames.Text = "";
+            // 
+            // button_ToAccept
+            // 
+            this.button_ToAccept.Location = new System.Drawing.Point(118, 21);
+            this.button_ToAccept.Name = "button_ToAccept";
+            this.button_ToAccept.Size = new System.Drawing.Size(75, 23);
+            this.button_ToAccept.TabIndex = 4;
+            this.button_ToAccept.Text = "Принять";
+            this.button_ToAccept.UseVisualStyleBackColor = true;
+            this.button_ToAccept.Click += new System.EventHandler(this.button_ToAccept_Click);
+            // 
+            // button_GenerateTest
+            // 
+            this.button_GenerateTest.Location = new System.Drawing.Point(12, 152);
+            this.button_GenerateTest.Name = "button_GenerateTest";
+            this.button_GenerateTest.Size = new System.Drawing.Size(230, 52);
+            this.button_GenerateTest.TabIndex = 5;
+            this.button_GenerateTest.Text = "Сгенерировать тест";
+            this.button_GenerateTest.UseVisualStyleBackColor = true;
+            this.button_GenerateTest.Click += new System.EventHandler(this.button_GenerateTest_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Кол-во вопросов";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Путь сохранения файла";
+            // 
+            // FormTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 212);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button_GenerateTest);
+            this.Controls.Add(this.button_ToAccept);
+            this.Controls.Add(this.ListOfSurnames);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.WritePath);
+            this.Controls.Add(this.NumberOfQuestions);
+            this.Name = "FormTest";
+            this.Text = "Генератор тестов";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NumberOfQuestions;
+        private System.Windows.Forms.TextBox WritePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox ListOfSurnames;
+        private System.Windows.Forms.Button button_ToAccept;
+        private System.Windows.Forms.Button button_GenerateTest;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Test/Test/FormTest.cs
@@ -0,0 +1,97 @@
+﻿using System.IO;
+using System;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class FormTest : Form
+    {
+        StreamReader reader;//прикол для чтения строк файла(понадобится ниже)
+        int number_of_questions = 0;
+
+        public FormTest()
+        {
+            InitializeComponent();
+            CenterToScreen();
+            AutoSize = true;
+            MaximizeBox = false;//нельзя развернуть окно
+            FormBorderStyle = FormBorderStyle.Fixed3D;//нельзя изменить размер окна
+            button_GenerateTest.AutoSize = true;
+        }
+
+        //функция позволяет миксовать вопросы в файле(костыли,но больше я пока не придумал)
+        void MixQuestion()
+        {
+            reader.Close();
+            string dest = Directory.GetCurrentDirectory() + @"\Test.txt";
+            var encoding = System.Text.Encoding.UTF8;
+            var lines = File.ReadAllLines(dest);
+            var rand = new Random();
+
+            for (int i = lines.Length - 1; i > 0; i--)
+            {
+                int j = rand.Next(i);
+
+                var temp = lines[i - 1];
+                lines[i - 1] = lines[j];
+                lines[j] = temp;
+            }
+            File.WriteAllLines(dest, lines, encoding);
+        }
+
+        //по факту,просто создание экземляра и вызов функции ниже
+        void StartTest()
+        {
+            var encoding = System.Text.Encoding.GetEncoding(1251);
+            try
+            {
+                // Создание экземпляра StreamReader для чтения из файла
+                reader = new StreamReader(Directory.GetCurrentDirectory() + @"\Test.txt", encoding);//место где лежит список заданий
+            }
+            catch (Exception message)
+            {   // Отчет о всех ошибках:
+                MessageBox.Show(message.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            ReadNextQuestion();
+        }
+
+        //функция позволяющая выбрать след вопрос
+        void ReadNextQuestion()
+        {
+            String[] tmp = reader.ReadLine().Split('^');
+            int index = 0;
+            label1.Text = tmp[index++];
+        }
+
+        //та самая кнопка в коорой происходит кривая костылявая но магия
+        private void button_GenerateTest_Click(object sender, EventArgs e)
+        {
+            string Path = WritePath.Text;
+            string writePath = @""+Path+".txt";//создание файла по пути написанному пользователем в WritePath
+
+            /*
+            Сталкнулся с небольшой проблемой в плане работы с расширением .docx(расширение word-a).Суть проблемы в том
+            что прога создаёт пишет(вроде как),но походу как-то криво,ибо открыть файл и проверить
+            что он туда записал невозможно(word жалуется).
+            */
+
+            StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default);
+
+            for (int i = 0; i < number_of_questions; i++)//записть нужного кол-ва вопросов в файл(пока txt)
+            {
+                StartTest();
+                MixQuestion();
+
+                sw.Write(label1.Text);
+                sw.Write("\n");
+            }
+            sw.Close();
+        }
+
+        private void button_ToAccept_Click(object sender, EventArgs e)
+        {
+            number_of_questions = Convert.ToInt32(NumberOfQuestions.Text);
+            MessageBox.Show("Кол-во вопросов принято!!!");
+        }
+    }
+}
